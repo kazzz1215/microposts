b672d9f00f368903aa4ba56f70e446068c5ea867
kazuhisaokada <kazzz1215@gmail.com>
2016-11-07 14:22:53 +0000

    kadai1 add user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update]
+  before_action :check_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -18,9 +21,34 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'プロフィールを更新しました'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :area, :profile, :password, :password_confirmation)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def check_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      flash[:danger] = '不正なアクセスです'
+      redirect_to root_path
+    end
+  end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,6 @@ module SessionsHelper
    end
     
     def store_location
-       session[:forwarding_url] = request.ural if request.get? 
+       session[:forwarding_url] = request.url if request.get? 
     end
 end
